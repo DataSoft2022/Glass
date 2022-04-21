04029beba8adff453a6d6953e0505b8f36523860
Ahmed Abdulrahman <ahmed751995@riseup.net>
2022-04-21 11:42:10 +0200

    multiple stock

---
+++ glass/api.py
@@ -0,0 +1,22 @@
+import frappe
+import copy
+
+@frappe.whitelist()
+def multi_stock_entry(doc):
+    stock_en = frappe.get_doc('Stock Entry', doc)
+
+    s_warehouses = {}
+    for item in stock_en.items:
+        if item.s_warehouse in s_warehouses.keys():
+            s_warehouses[item.s_warehouse].append(copy.copy(item))
+        else:
+            s_warehouses[item.s_warehouse] = [copy.copy(item)]
+
+    for warehouse in s_warehouses:
+        n_stock = copy.copy(frappe.get_doc('Stock Entry', doc))
+        n_stock.name = ''
+        n_stock.items = s_warehouses[warehouse]
+        n_stock.save()
+    
+    frappe.db.commit()
+    return 'done'
