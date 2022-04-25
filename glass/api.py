885dd17a6b2cb512b8e162a3c550f4c763a8617a
Ahmed Abdulrahman <ahmed751995@riseup.net>
2022-04-25 14:44:18 +0200

    improvements

--- glass/api.py
+++ glass/api.py
@@ -2,20 +2,21 @@ import frappe
 import copy
 
 @frappe.whitelist()
-def multi_stock_entry(doc):
+def multi_stock_entry(doc, split_key):
     stock_en = frappe.get_doc('Stock Entry', doc)
-
-    s_warehouses = {}
+    print(split_key)
+    split_items = {}
     for item in stock_en.items:
-        if item.s_warehouse in s_warehouses.keys():
-            s_warehouses[item.s_warehouse].append(copy.copy(item))
+        if item.get(split_key) in split_items.keys():
+            split_items[item.get(split_key)].append(copy.copy(item))
         else:
-            s_warehouses[item.s_warehouse] = [copy.copy(item)]
+            split_items[item.get(split_key)] = [copy.copy(item)]
 
-    for warehouse in s_warehouses:
+    print(split_items)
+    for k in split_items:
         n_stock = copy.copy(frappe.get_doc('Stock Entry', doc))
         n_stock.name = ''
-        n_stock.items = s_warehouses[warehouse]
+        n_stock.items = split_items[k]
         n_stock.save()
 
     stock_en.delete()
