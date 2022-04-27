aeeefa0f687b9dfc9dbf146ad0caa8f0cc711ca2
Ahmed Abdulrahman <ahmed.m@datasofteg.com>
2022-04-27 10:06:04 +0200

    fix idx eroor and add operation field

--- glass/api.py
+++ glass/api.py
@@ -17,6 +17,9 @@ def multi_stock_entry(doc, split_key):
         n_stock = copy.copy(frappe.get_doc('Stock Entry', doc))
         n_stock.name = ''
         n_stock.items = split_items[k]
+        for i in range(len(n_stock.items)):
+            n_stock.items[i].idx = i + 1
+        n_stock.operation = n_stock.items[0].operation
         n_stock.save()
 
     stock_en.delete()
